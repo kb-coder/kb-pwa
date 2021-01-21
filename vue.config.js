a88c58d8d705118c2a105e925903138ce16fc445
Karen Baney <Karen_Baney@SHAMROCKFOODS.com>
2021-01-21 12:47:13 -0700

    linting and remove boilerplate component

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,15 @@ module.exports = {
     } else if (process.env.NODE_ENV === 'test') {
       config.devtool = 'cheap-module-eval-source-map'
     }
+  },
+  pwa: {
+    name: 'KB PWA App',
+    shorName: 'KB PWA',
+    themeColor: '#400080',
+    msTileColor: '#800080',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      cleanupOutdatedCaches: true // cleans up cache that is out dated because of a previous version of Workbox.
+    }
   }
 }
