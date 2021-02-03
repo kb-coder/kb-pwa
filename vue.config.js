23ef37dc316c86b096ccb87f314b9674bb1c8821
Karen Baney <Karen_Baney@SHAMROCKFOODS.com>
2021-02-03 08:15:09 -0700

    fix start_url
    Fixes the start_url so devices (iOS, Android) actually see something. Otherwise they see blank page.

--- vue.config.js
+++ vue.config.js
@@ -16,11 +16,21 @@ module.exports = {
     themeColor: '#400080',
     msTileColor: '#800080',
     manifestOptions: {
-      start_url: './index.html'
+      start_url: '/' // The default vue pwa plugin uses '.'. Also many examples on line show '/index.html'. Either of these will
+      // result in a blank screen when installed on iOS or Android, but they will work fine on a PC. The '/' must also be included
+      // in the router. This will meet the requirements of a good start_url and it will install and display correctly on the devices.
     },
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       cleanupOutdatedCaches: true // cleans up cache that is outdated because of a previous version of Workbox.
     }
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'KB Coder PWA App' // Add title to the app bar when installed as PWA.
+        return args
+      })
   }
 }
